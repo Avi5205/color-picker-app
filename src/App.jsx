ef6234934e289af211e144d3aa082c76e2f413ff
Avinash <akgpcamp@gmail.com>
2025-01-23 17:06:12 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [background, setBackground] = useState("#fff");
+  const colors = [
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
+    "#ffff00",
+    "#ff00ff",
+    "#00ffff",
+  ];
 
+  const handleColorChange = (color) => {
+    setBackground(color);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ background }} className="App">
+      <h1>Color Picker</h1>
+      <div className="color-palette">
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={() => handleColorChange(color)}
+          ></div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="custom-color-picker">
+        <input
+          type="color"
+          value={background}
+          onChange={(e) => setBackground(e.target.value)}
+        ></input>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
